21d1fbc890efcc2db93ff9716fbc53b4e9493cfa
MvdS <postaanmartin@gmail.com>
2025-06-06 15:31:57 +0200

    Example debug script

--- examples/example.py
+++ examples/example.py
@@ -1,1 +1,13 @@
-import ipdab
+# script_to_debug.py
+import time
+import ipdb
+
+print("Starting script...")
+
+time.sleep(1)
+
+ipdb.set_trace()  # or use ipdb.Debugger().set_trace()
+
+x = 10
+y = x * 2
+print("Result:", y)
